f7af9320f5661006745364a6936574fcf3edec95
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2021-12-07 22:37:20 +0530

    Final Commit

--- tictactoe.c
+++ tictactoe.c
@@ -128,13 +128,17 @@ void findemptyposition()
     int k = 1;
 
     isempty = 0;
-    printf("Available Positions :  ");
+    // printf("Available Positions :  ");
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             if (board[i][j] == 0)
             {
+                if(isempty == 0)
+                {
+                    printf("Available Positions :  ");
+                }
                 printf("%d ", k);
                 isempty = 1;
             }
